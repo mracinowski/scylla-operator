db1cd25818a5db40ea5f7b4ee67498d7899e12ab
ttytek <ttytek@gmail.com>
2025-05-13 18:02:40 +0200

    Fix tests

--- pkg/analyze/front/print_test.go
+++ pkg/analyze/front/print_test.go
@@ -4,77 +4,33 @@ import (
 	"bytes"
 	"github.com/google/go-cmp/cmp"
 	"github.com/scylladb/scylla-operator/pkg/analyze/symptoms"
-	//v1 "k8s.io/api/core/v1"
-	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"testing"
 )
 
 func TestPrint(t *testing.T) {
-
-	/*
-		stdout := os.Stdout
-		r, w, _ := os.Pipe()
-		os.Stdout = w
-
-		defer func() {
-			os.Stdout = stdout
-		}()
-
-		symptom := symptoms.NewSymptom("name", []string{"diag1", "diag2"}, []string{"sugg1", "sugg2"}, nil)
-		resources := map[string]any{
-			"pod": &v1.Pod{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "pod1",
-				},
-			},
-			"serviceAccount": &v1.ServiceAccount{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "serviceAccount1",
-				},
-			},
-		}
-		issue := symptoms.NewIssue(&symptom, resources)
-
-		const (
-			bold       = "\033[1m"
-			red        = "\033[31m"
-			yellow     = "\033[93m"
-			blue       = "\033[34m"
-			resetStyle = "\033[0m"
-		)
-		expected := bold + red + "name" + resetStyle + "\n\n"
-		expected += red + "diag1" + resetStyle + "\n" + red + "diag2" + resetStyle + "\n\n"
-		expected += blue + "sugg1" + resetStyle + "\n" + blue + "sugg2" + resetStyle + "\n\n"
-		expected += yellow + "Related resources:" + resetStyle + "\n"
-		expected += yellow + "             pod -> pod1" + resetStyle + "\n"
-		expected += yellow + "  serviceAccount -> serviceAccount1" + resetStyle + "\n\n"
-
-		expected += bold + red + "name" + resetStyle + "\n"
-
-		Print(issue, false)
-		Print(issue, true)
-
-		var buf bytes.Buffer
-		w.Close()
-		buf.ReadFrom(r)
-		r.Close()
-		output := buf.String()
-
-		if output != expected {
-			t.Errorf("expected %q got %q", expected, output)
-		}
-	*/
-
 	tt := []struct {
 		name     string
-		sym      symptoms.Symptom
-		res      map[string]any
+		symptom      symptoms.Symptom
+		resources      map[string]any
 		expected string
 	}{
 		{
 			name: "Simple issue",
-			sym:  symptoms.NewSymptom("name", []string{"diag1", "diag2"}, []string{"sugg1", "sugg2"}, nil),
-			res: nil,
+			symptom : symptoms.NewSymptom("name", []string{"diag1", "diag2"}, []string{"sugg1", "sugg2"}, nil),
+			resources : map[string]any{
+				"pod": &v1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "pod1",
+					},
+				},
+				"serviceAccount": &v1.ServiceAccount{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "serviceAccount1",
+					},
+				},
+			},
 			expected: "",
 		},
 	}
@@ -82,7 +38,7 @@ func TestPrint(t *testing.T) {
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
-			issue := symptoms.NewIssue(&(tc.sym), tc.res)
+			issue := symptoms.NewIssue(&(tc.symptom), tc.resources)
 			var buf bytes.Buffer
 			Print(&buf, issue)
 			got := buf.String()
